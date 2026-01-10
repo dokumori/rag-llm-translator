2f25a64eb10c792ccfc2c3bab6d2af97d922ab48
Mori Sugimoto <mori@diasporan.net>
2026-01-10 23:53:38 +0100

    Downloads the translated strings to be ingested as translation memory

---
+++ bin/demo_prep.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+SOURCE_DIR="data/glossary"
+PO_FILE="${SOURCE_DIR}/drupal-11.0.6.ja.po"
+PO_URL="https://ftp.drupal.org/files/translations/all/drupal/drupal-11.0.6.ja.po"
+
+# Download the file if it's not already there
+if [ ! -f "$PO_FILE" ]; then
+  echo "📥 Downloading Drupal core translations for the demo..."
+  curl -L -o "$PO_FILE" "$PO_URL"
+else
+  echo "✅ The file `$PO_FILE` already exists."
+fi
